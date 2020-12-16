6aa00a9180f763c24d4487a6b8d98726938f8b74
程序员阿克曼 <1985849912@qq.com>
2020-12-16 20:03:27 +0800

    Add manual button callback

--- server/app.py
+++ server/app.py
@@ -8,15 +8,24 @@ app = Flask(__name__)
 def index():
     return flask.render_template('./index.html')
 
+
 @app.route('/voice', methods=['POST'])
 def voice():
     app.logger.info(flask.request.data)
     return 'success'
 
+
 @app.route('/track', methods=['POST'])
 def track():
     app.logger.info(flask.request.data)
     return 'success'
+
+
+@app.route('/shoot', methods=['POST'])
+def shoot():
+    app.logger.info('shoot')
+    return "success"
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80, debug=True)
